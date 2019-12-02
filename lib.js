80a6554ecd24a1b3608725222896b25c21214bf0
Connor Clark <cjamcl@gmail.com>
2019-12-01 18:43:38 -0600

    update

---
+++ lib.js
@@ -0,0 +1,63 @@
+// Lifted from source-map-explorer.
+/** Calculate the number of bytes contributed by each source file */
+function computeFileSizeMapOptimized(sourceMapData) {
+  const { consumer, content } = sourceMapData;
+  const lines = content.split('\n');
+  /** @type {Record<string, number>} */
+  const files = {};
+  let mappedBytes = 0;
+
+  consumer.computeColumnSpans();
+
+  consumer.eachMapping(({ source, generatedLine, generatedColumn, lastGeneratedColumn }) => {
+    // Lines are 1-based
+    const line = lines[generatedLine - 1];
+    if (line === null) {
+      // throw new AppError({
+      //   code: 'InvalidMappingLine',
+      //   generatedLine: generatedLine,
+      //   maxLine: lines.length,
+      // });
+    }
+
+    // Columns are 0-based
+    if (generatedColumn >= line.length) {
+      // throw new AppError({
+      //   code: 'InvalidMappingColumn',
+      //   generatedLine: generatedLine,
+      //   generatedColumn: generatedColumn,
+      //   maxColumn: line.length,
+      // });
+      return;
+    }
+
+    let mappingLength = 0;
+    if (lastGeneratedColumn !== null) {
+      if (lastGeneratedColumn >= line.length) {
+        // throw new AppError({
+        //   code: 'InvalidMappingColumn',
+        //   generatedLine: generatedLine,
+        //   generatedColumn: lastGeneratedColumn,
+        //   maxColumn: line.length,
+        // });
+        return;
+      }
+      mappingLength = lastGeneratedColumn - generatedColumn + 1;
+    } else {
+      mappingLength = line.length - generatedColumn;
+    }
+    files[source] = (files[source] || 0) + mappingLength;
+    mappedBytes += mappingLength;
+  });
+
+  // Don't count newlines as original version didn't count newlines
+  const totalBytes = content.length - lines.length + 1;
+
+  return {
+    files,
+    unmappedBytes: totalBytes - mappedBytes,
+    totalBytes,
+  };
+}
+
+module.exports.computeFileSizeMapOptimized = computeFileSizeMapOptimized;
--- run.js
+++ run.js
@@ -1,86 +1,125 @@
 const fs = require('fs');
 const path = require('path');
-const {execFileSync} = require('child_process');
+const { execFileSync } = require('child_process');
+const { SourceMapConsumer } = require('source-map');
+const { computeFileSizeMapOptimized } = require('./lib.js');
 
 const mode = process.argv[2];
 const dir = 'data/' + process.argv[3];
-const urls = process.argv.slice(4);
+const urlAndNames = process.argv.slice(4).map(nameAndUrl => {
+  const split = nameAndUrl.split('=', 2);
+  return { name: split[0], url: split[1] };
+});
+// assume all urls have same origin.
+const origin = new URL(urlAndNames[0].url).origin;
 
 function sanitize(str) {
   return str.replace(/[^a-zA-Z0-9]/g, '-').substring(0, 100);
 }
 
-function sameOrigin(url1, url2) {
-  return new URL(url1).origin === new URL(url2).origin;
+function sameOrigin(url) {
+  return new URL(url).origin === origin;
 }
 
-if (mode === 'collect') {
-  fs.mkdirSync(dir, {recursive: true});
-  
-  for (const url of urls) {
-    const urlSanitized = sanitize(url);
-    const outputFolder = `${dir}/${urlSanitized}`;
-    if (fs.existsSync(outputFolder)) continue;
-    console.log('collect', url);
-    
-    execFileSync('node', [
-      '/Users/Connor/code/lighthouse/lighthouse-cli',
-      url,
-      '-GA=' + `${outputFolder}/artifacts`,
-      '--output=json',
-      '--output=html',
-      '--output-path=' + outputFolder + '/lh', // LH doesn't let you juut specify a folder ...
-    ]);
-  }
+async function main() {
+  if (mode === 'collect') {
+    fs.mkdirSync(dir, { recursive: true });
 
-  const scriptData = {};
-  for (const url of urls) {
-    console.log('===========', url);
-    
-    const urlSanitized = sanitize(url);
-    const outputFolder = `${dir}/${urlSanitized}`;
-    const artifacts = require(path.resolve(outputFolder, 'artifacts', 'artifacts.json'));
-    const lhr = require(path.resolve(outputFolder, 'lh.report.json'));
+    for (const { url } of urlAndNames) {
+      const urlSanitized = sanitize(url);
+      const outputFolder = `${dir}/${urlSanitized}`;
+      if (fs.existsSync(outputFolder)) continue;
+      console.log('collect', url);
 
-    for (const ScriptElement of artifacts.ScriptElements) {
-      if (!ScriptElement.src) continue;
-      if (!scriptData[ScriptElement.src]) {
-        scriptData[ScriptElement.src] = {
-          scriptUrl: ScriptElement.src,
-          content: ScriptElement.content || '',
-          seen: [],
-        };
+      execFileSync('node', [
+        __dirname + '/../lighthouse/lighthouse-cli',
+        url,
+        '-GA=' + `${outputFolder}/artifacts`,
+        '--output=json',
+        '--output=html',
+        '--output-path=' + outputFolder + '/lh', // LH doesn't let you juut specify a folder ...
+      ]);
+    }
+
+    const scriptData = {};
+    for (const { url } of urlAndNames) {
+      const urlSanitized = sanitize(url);
+      const outputFolder = `${dir}/${urlSanitized}`;
+      const artifacts = require(path.resolve(outputFolder, 'artifacts', 'artifacts.json'));
+      // const lhr = require(path.resolve(outputFolder, 'lh.report.json'));
+
+      for (const ScriptElement of artifacts.ScriptElements) {
+        if (!ScriptElement.src) continue;
+        if (!scriptData[ScriptElement.src]) {
+          scriptData[ScriptElement.src] = {
+            scriptUrl: ScriptElement.src,
+            content: ScriptElement.content || '',
+            seen: [],
+          };
+        }
+
+        scriptData[ScriptElement.src].seen.push(url);
       }
-
-      scriptData[ScriptElement.src].seen.push(url);
-    }
-    for (const SourceMap of artifacts.SourceMaps) {
-      if (!SourceMap.scriptUrl) continue;
-      scriptData[SourceMap.scriptUrl].sourceMapUrl = SourceMap.sourceMapUrl;
-      scriptData[SourceMap.scriptUrl].map = SourceMap.map;
-    }
-  }
-
-  const jsOutFolder = `${dir}/js`;
-  const js3pOutFolder = `${dir}/js-3p`;
-  fs.mkdirSync(jsOutFolder, {recursive: true});
-  fs.mkdirSync(js3pOutFolder, {recursive: true});
-  
-  for (const [scriptUrl, data] of Object.entries(scriptData)) {
-    const outFolder = sameOrigin(scriptUrl, urls[0]) ? jsOutFolder : js3pOutFolder;
-    fs.writeFileSync(`${outFolder}/${sanitize(scriptUrl)}.js`, data.content);
-    if (data.sourceMapUrl) {
-      fs.writeFileSync(`${outFolder}/${sanitize(data.sourceMapUrl)}.js.map`, JSON.stringify(data.map, null, 2));
+      for (const SourceMap of artifacts.SourceMaps) {
+        if (!SourceMap.scriptUrl) continue;
+        scriptData[SourceMap.scriptUrl].sourceMapUrl = SourceMap.sourceMapUrl;
+        scriptData[SourceMap.scriptUrl].map = SourceMap.map;
+      }
     }
 
-    if (sameOrigin(scriptUrl, urls[0])) console.dir(scriptUrl);
-  }
+    const jsOutFolder = `${dir}/js`;
+    const js3pOutFolder = `${dir}/js-3p`;
+    fs.mkdirSync(jsOutFolder, { recursive: true });
+    fs.mkdirSync(js3pOutFolder, { recursive: true });
 
-  console.log('====== all')
-  const datas = Object.values(scriptData).sort((a, b) => b.content.length - a.content.length);
-  for (const data of datas) {
-    if (!sameOrigin(data.scriptUrl, urls[0])) continue;
+    for (const [scriptUrl, data] of Object.entries(scriptData)) {
+      const outFolder = sameOrigin(scriptUrl) ? jsOutFolder : js3pOutFolder;
+      fs.writeFileSync(`${outFolder}/${sanitize(scriptUrl)}.js`, data.content);
+      if (data.sourceMapUrl) {
+        fs.writeFileSync(`${outFolder}/${sanitize(data.sourceMapUrl)}.js.map`, JSON.stringify(data.map, null, 2));
+      }
+    }
 
-    console.log(Math.round(data.content.length / 1024), data.scriptUrl, ...data.seen);
+    console.log('====== bundles');
+    for (const data of Object.values(scriptData).sort((a, b) => b.content.length - a.content.length)) {
+      if (!data.map) continue;
+
+      console.log('______', data.scriptUrl, data.content.length);
+      const consumer = await new SourceMapConsumer(data.map);
+      const files = computeFileSizeMapOptimized({ consumer, content: data.content }).files;
+      const sortedFiles = Object.entries(files).sort((a, b) => b[1] - a[1]);
+      const largest = sortedFiles.slice(0, 5);
+      const rest = sortedFiles.slice(5);
+      for (const [file, size] of largest) {
+        console.log(file, size);
+      }
+      if (rest.length) {
+        console.log(`${rest.length} more ...`);
+      }
+    }
+
+    console.log('====== javascript size and pages')
+    const table = {};
+
+    function trimSameOrigin(url) {
+      if (sameOrigin(url)) {
+        const url_ = new URL(url);
+        return url_.pathname + url_.search;
+      } else {
+        return url;
+      }
+    }
+
+    for (const data of Object.values(scriptData).sort((a, b) => b.content.length - a.content.length)) {
+      if (!sameOrigin(data.scriptUrl)) continue;
+
+      table[trimSameOrigin(data.scriptUrl)] = {
+        'size (KB)': Math.round(data.content.length / 1024),
+        pages: data.seen.map(url => urlAndNames.find(un => un.url === url).name).sort().join(', '),
+      };
+    }
+    console.table(table);
   }
 }
+
+main();
